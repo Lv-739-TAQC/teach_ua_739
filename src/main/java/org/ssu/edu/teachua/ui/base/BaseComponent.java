766b9eadf4e242d240ede2afecec609ff1f27cf6
Liubomyr Halamaha <galamagal86@gmail.com>
2023-02-15 13:55:24 +0200

    update BaseComponent

--- src/main/java/org/ssu/edu/teachua/ui/base/BaseComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/base/BaseComponent.java
@@ -1,9 +1,15 @@
 package org.ssu.edu.teachua.ui.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 
 public abstract class BaseComponent extends Base {
-    public BaseComponent(WebDriver driver) {
+    protected WebElement componentRoot;
+    public BaseComponent(WebDriver driver, WebElement node) {
         super(driver);
+        componentRoot = node;
+        PageFactory.initElements(new DefaultElementLocatorFactory(componentRoot), this);
     }
 }
