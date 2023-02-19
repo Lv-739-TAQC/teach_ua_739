68192f709502a56508c4d5849387dec3bcfb230a
Ira Zhurakivska <zhurakivskairen@ukr.net>
2023-02-19 18:22:16 +0200

    added selectors to log in

---
+++ src/main/java/org/ssu/edu/teachua/ui/components/modal/SignUpComponent.java
@@ -0,0 +1,9 @@
+package org.ssu.edu.teachua.ui.components.modal;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.ssu.edu.teachua.ui.base.BaseComponent;
+
+public class RegistrationComponent extends BaseComponent {
+    public RegistrationComponent (WebDriver driver, WebElement node) {super(driver, node);}
+}
