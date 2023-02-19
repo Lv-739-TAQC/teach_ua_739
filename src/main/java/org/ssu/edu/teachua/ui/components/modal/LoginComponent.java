52fee5e12929d066208cd3d647f4aec475a9a3ab
Ira Zhurakivska <zhurakivskairen@ukr.net>
2023-02-19 20:03:32 +0200

    added selectors to sign up and log in

--- src/main/java/org/ssu/edu/teachua/ui/components/modal/LoginComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/modal/LoginComponent.java
@@ -2,10 +2,36 @@ package org.ssu.edu.teachua.ui.components.modal;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.ssu.edu.teachua.ui.base.BaseComponent;
 
 public class LoginComponent extends BaseComponent {
+    @FindBy(how = How.XPATH, using = "//input[@id='basic_email']")
+    private WebElement emailField;
+    @FindBy (how = How.XPATH, using = "//input[@id='basic_password']")
+    private WebElement passwordField;
+    @FindBy (how = How.XPATH, using = "//*[@class='ant-btn ant-btn-default login-button']")
+    private WebElement logInButton;
+    @FindBy (how = How.XPATH, using = "//div[@class='ant-modal modal-login']//button[contains(@aria-label,'Close')]")
+    private WebElement closeButton;
+
     public LoginComponent(WebDriver driver, WebElement node) {
         super(driver, node);
     }
+
+    public LoginComponent enterEmail(String email) {
+        emailField.sendKeys(email);
+        return this;
+    }
+    public LoginComponent enterPassword(String password) {
+        passwordField.sendKeys(password);
+        return this;
+    }
+    public LoginComponent clickLoginButton() {
+        logInButton.click();
+        return this;
+    }
+
 }
+
