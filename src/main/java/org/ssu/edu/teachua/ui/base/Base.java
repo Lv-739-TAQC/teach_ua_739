ec1423b1184e7948a2f97b611704dae91b86187f
Maksym <86326168+Maksym637@users.noreply.github.com>
2023-02-22 15:11:26 +0200

    Profile, EditProfile and News implementation (#5)
    * Profile and EditProfile implementation
    
    * NewsPage and ViewNewsPage are done

--- src/main/java/org/ssu/edu/teachua/ui/base/Base.java
+++ src/main/java/org/ssu/edu/teachua/ui/base/Base.java
@@ -1,19 +1,47 @@
 package org.ssu.edu.teachua.ui.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.List;
 
 public abstract class Base {
+
+    private static final int EXPLICIT_WAIT = 100;
+
     protected WebDriver driver;
+    protected WebDriverWait wait;
 
     public Base(WebDriver driver) {
         this.driver = driver;
+        wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
         PageFactory.initElements(driver, this);
     }
 
+    public void open(String url) {
+        driver.get(url);
+    }
+
+    protected WebElement waitForElementToAppear(WebElement element) {
+        return wait.until(ExpectedConditions.visibilityOf(element));
+    }
+
+    protected WebElement waitForElementToBeClickable(WebElement element) {
+        return wait.until(ExpectedConditions.elementToBeClickable(element));
+    }
+
+    protected List<WebElement> waitForElementsToAppear(List<WebElement> elements) {
+        return wait.until(ExpectedConditions.visibilityOfAllElements(elements));
+    }
+
+    // temporary falling asleep :
     protected void sleep(long s) {
         try {
-            Thread.sleep(s*1000);
+            Thread.sleep(s * 1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
---
+++ src/main/java/org/ssu/edu/teachua/ui/pages/view/ViewNewsPage.java
@@ -0,0 +1,65 @@
+package org.ssu.edu.teachua.ui.pages.view;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import org.ssu.edu.teachua.ui.base.BasePage;
+import org.ssu.edu.teachua.ui.components.card.NewsCardComponent;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ViewNewsPage extends BasePage {
+
+    @FindBy(how = How.XPATH, using = "//div[@id='major-title']")
+    private WebElement newsTitle;
+
+    @FindBy(how = How.XPATH, using = "//div[@id='date']")
+    private WebElement newsDate;
+
+    @FindBy(how = How.XPATH, using = "//div[@id='description']")
+    private WebElement newsDescription;
+
+    @FindBy(how = How.XPATH, using = "//div[@class='other-news']")
+    private WebElement otherNewsTitle;
+
+    @FindBy(how = How.XPATH, using = "//div[@class='slick-track']/*[position() <= 3]")
+    private List<WebElement> otherNewsNodes;
+
+    public ViewNewsPage(WebDriver driver) {
+        super(driver);
+    }
+
+    public List<NewsCardComponent> getCardsWithOtherNews() {
+        List<NewsCardComponent> otherNewsComponents = new ArrayList<>();
+        for (WebElement otherNewsNode : waitForElementsToAppear(otherNewsNodes)) {
+            otherNewsComponents.add(new NewsCardComponent(driver, otherNewsNode));
+        }
+        return otherNewsComponents;
+    }
+
+    public int getCountAllOtherNews() {
+        return getCardsWithOtherNews().size();
+    }
+
+    public String getNewsTitle() {
+        return newsTitle.getText();
+    }
+
+    public String getNewsDate() {
+        return newsDate.getText();
+    }
+
+    public String getNewsDescription() {
+        return newsDescription.getText();
+    }
+
+    public String getOtherNewsTitle() {
+        return otherNewsTitle.getText();
+    }
+
+    public NewsCardComponent chooseCertainOtherNews(int componentNumber) {
+        return getCardsWithOtherNews().get(componentNumber);
+    }
+}
