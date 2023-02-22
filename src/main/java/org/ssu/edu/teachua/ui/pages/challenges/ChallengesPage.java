63d2b34684147b19ee8e0be74f573956b59aed1e
Zhanna Stadnytska <janelischuk@gmail.com>
2023-02-22 14:44:43 +0200

    xPath upd

--- src/main/java/org/ssu/edu/teachua/ui/pages/challenges/ChallengesPage.java
+++ src/main/java/org/ssu/edu/teachua/ui/pages/challenges/ChallengesPage.java
@@ -11,29 +11,29 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class ChallengesPage extends BasePage {
-    @FindBy(how = How.XPATH, using = ".//*[@class='back-btn']")
+    @FindBy(how = How.XPATH, using = "//*[@class='back-btn']")
     private WebElement tasksBtn;
-    @FindBy(how = How.XPATH, using = ".//a[@href='/dev/admin/addChallenge']")
+    @FindBy(how = How.XPATH, using = "//a[@href='/dev/admin/addChallenge']")
     private WebElement addChallengeBtn;
-    @FindBy(how = How.XPATH, using = ".//span[contains(@class, 'ant-input-group')]//*[@type='text']")
+    @FindBy(how = How.XPATH, using = "//span[contains(@class, 'ant-input-group')]//*[@type='text']")
     private WebElement searchField;
-    @FindBy(how = How.XPATH, using = ".//span[@class='ant-input-group-addon']")
+    @FindBy(how = How.XPATH, using = "//span[@class='ant-input-group-addon']")
     private WebElement searchBtn;
-    @FindBy(how = How.XPATH, using = "(.//*[@class='ant-table-cell']//a)[1]")
+    @FindBy(how = How.XPATH, using = "(//*[@class='ant-table-cell']//a)[1]")
     private WebElement challengeId;
-    @FindBy(how = How.XPATH, using = "(.//*[@class='ant-table-cell']//a)[2]")
+    @FindBy(how = How.XPATH, using = "(//*[@class='ant-table-cell']//a)[2]")
     private WebElement challengeSortNumber;
-    @FindBy(how = How.XPATH, using = "(.//*[@class='ant-table-cell']//a)[3]")
+    @FindBy(how = How.XPATH, using = "(//*[@class='ant-table-cell']//a)[3]")
     private WebElement challengeName;
-    @FindBy(how = How.XPATH, using = "(.//*[@class='ant-table-cell']//a)[4]")
+    @FindBy(how = How.XPATH, using = "(//*[@class='ant-table-cell']//a)[4]")
     private WebElement challengeTitle;
-    @FindBy(how = How.XPATH, using = ".//*[@title='Previous Page']")
+    @FindBy(how = How.XPATH, using = "//*[@title='Previous Page']")
     private WebElement previousPage;
-    @FindBy(how = How.XPATH, using = ".//*[@title='Next Page']")
+    @FindBy(how = How.XPATH, using = "//*[@title='Next Page']")
     private WebElement nextPage;
-    @FindBy(how = How.XPATH, using = ".//*[@title='Next 5 Pages']")
+    @FindBy(how = How.XPATH, using = "//*[@title='Next 5 Pages']")
     private WebElement stepOverNextFivePages;
-    @FindBy(how = How.XPATH, using = ".//*[contains(@class, 'level-0 editable-row')]")
+    @FindBy(how = How.XPATH, using = "//*[contains(@class, 'level-0 editable-row')]")
     private List<WebElement> challengeLocators;
     private List<ChallengeComponent> challengeComponents;
 
