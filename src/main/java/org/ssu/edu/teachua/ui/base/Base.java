a644f41b5e6ede9a5e42c7107df4567cd9052aa1
DenisPitsul <denia.pitsul@gmail.com>
2023-02-22 15:14:22 +0200

    Tasks, AddTask, EditTask page objects implementation (#7)
    * Tasks, AddTask, EditTask page objects implementation

--- src/main/java/org/ssu/edu/teachua/ui/base/Base.java
+++ src/main/java/org/ssu/edu/teachua/ui/base/Base.java
@@ -1,6 +1,7 @@
 package org.ssu.edu.teachua.ui.base;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.PageFactory;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -15,9 +16,11 @@ public abstract class Base {
 
     protected WebDriver driver;
     protected WebDriverWait wait;
+    protected Actions actions;
 
     public Base(WebDriver driver) {
         this.driver = driver;
+        actions = new Actions(driver);
         wait = new WebDriverWait(driver, Duration.ofSeconds(EXPLICIT_WAIT));
         PageFactory.initElements(driver, this);
     }
--- src/main/java/org/ssu/edu/teachua/ui/pages/tasks/AddTaskPage.java
+++ src/main/java/org/ssu/edu/teachua/ui/pages/tasks/AddTaskPage.java
@@ -1,10 +1,78 @@
 package org.ssu.edu.teachua.ui.pages.tasks;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.ssu.edu.teachua.ui.base.BasePage;
+import org.ssu.edu.teachua.ui.components.date_picker.SelectDateComponent;
+import org.ssu.edu.teachua.ui.pages.view.ViewTaskPage;
 
 public class AddTaskPage extends BasePage {
+
+    protected static final String CHALLENGE_NAME_XPATH = "//div[@class='ant-select-item-option-content' and text()='%s']";
+
+    @FindBy(how = How.XPATH, using = "//input[@id='startDate']")
+    protected WebElement startDateInput;
+
+    @FindBy(how = How.XPATH, using = "//input[@id='picture']")
+    protected WebElement photoInput;
+
+    @FindBy(how = How.XPATH, using = "//input[@id='name']")
+    protected WebElement nameInput;
+
+    @FindBy(how = How.XPATH, using = "//label[text()='Заголовок']//parent::div//following-sibling::div//div[contains(@class, 'ql-editor')]/p")
+    protected WebElement titleInput;
+
+    @FindBy(how = How.XPATH, using = "//label[text()='Опис']//parent::div//following-sibling::div//div[contains(@class, 'ql-editor')]/p")
+    protected WebElement descriptionInput;
+
+    @FindBy(how = How.XPATH, using = "//input[@id='challengeId']")
+    protected WebElement challengeDropdown;
+
+    @FindBy(how = How.XPATH, using = "//span[text()='Зберегти']//parent::button")
+    protected WebElement saveBtn;
+
     public AddTaskPage(WebDriver driver) {
         super(driver);
+    }
+
+    public AddTaskPage selectStartDate(int day, int month, int year) {
+        startDateInput.click();
+        WebElement selectDateNode = driver.findElement(By.xpath("//div[contains(@class, 'ant-picker-dropdown')]"));
+        new SelectDateComponent(driver, selectDateNode).selectDate(day, month, year);
+        return this;
+    }
+
+    public AddTaskPage uploadPhoto(String photoPath) {
+        photoInput.sendKeys(photoPath);
+        return this;
+    }
+
+    public AddTaskPage typeName(String name) {
+        nameInput.sendKeys(name);
+        return this;
+    }
+
+    public AddTaskPage typeTitle(String title) {
+        titleInput.sendKeys(title);
+        return this;
+    }
+
+    public AddTaskPage typeDescription(String title) {
+        descriptionInput.sendKeys(title);
+        return this;
+    }
+
+    public AddTaskPage selectChallenge(String challenge) {
+        challengeDropdown.click();
+        driver.findElement(By.xpath(String.format(CHALLENGE_NAME_XPATH, challenge))).click();
+        return this;
+    }
+
+    public void clickSaveButton() {
+        saveBtn.click();
     }
 }
