e158a4485247bc1a64c6ffef6bb8e02810d3cdb4
Liubomyr Halamaha <galamagal86@gmail.com>
2023-02-23 15:44:15 +0200

    refactir

--- src/main/java/org/ssu/edu/teachua/ui/components/modal/AddLocationComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/modal/AddLocationComponent.java
@@ -8,12 +8,26 @@ import org.openqa.selenium.support.ui.Select;
 import org.ssu.edu.teachua.ui.base.BaseComponent;
 
 public class AddLocationComponent extends BaseComponent {
+    @FindBy(how = How.XPATH, using = ".//*[@id='name']")
+    private WebElement locationName;
+    @FindBy(how = How.XPATH, using = ".//input[@id='cityName']")
+    private WebElement locationCity;
+    @FindBy(how = How.XPATH, using = ".//input[@id='districtName']")
+    private WebElement locationDistrict;
+    @FindBy(how = How.XPATH, using = ".//input[@id='stationName']")
+    private WebElement locationSubway;
+    @FindBy(how = How.XPATH, using = ".//*[@id='address']")
+    private WebElement locationAddress;
+    @FindBy(how = How.XPATH, using = ".//*[@id='coordinates']")
+    private WebElement locationGC;
+    @FindBy(how = How.XPATH, using = ".//*[@id='phone']")
+    private WebElement locationPhone;
+    @FindBy(how = How.XPATH, using = ".//*[@type='submit' and contains(@class, 'flooded-button')]")
+    private WebElement addLocationToListButton;
+
     public AddLocationComponent(WebDriver driver, WebElement node) {
         super(driver, node);
     }
-
-    @FindBy(how = How.XPATH, using = ".//*[@id='name']")
-    private WebElement locationName;
 
     //    @Step("Add new location pop-up: enter location name")
     public AddLocationComponent enterLocationName(String locationName) {
@@ -23,18 +37,12 @@ public class AddLocationComponent extends BaseComponent {
         return this;
     }
 
-    @FindBy(how = How.XPATH, using = ".//input[@id='cityName']")
-    private WebElement locationCity;
-
     //    @Step("Add new location pop-up: select location city")
     public AddLocationComponent selectLocationCity(String city) {
         Select selectFromCityList = new Select(locationCity);
         selectFromCityList.selectByVisibleText(city);
         return this;
     }
-
-    @FindBy(how = How.XPATH, using = ".//input[@id='districtName']")
-    private WebElement locationDistrict;
 
     //     @Step("Add new location pop-up: select location district")
     public AddLocationComponent selectLocationDistrict(String district) {
@@ -43,18 +51,12 @@ public class AddLocationComponent extends BaseComponent {
         return this;
     }
 
-    @FindBy(how = How.XPATH, using = ".//input[@id='stationName']")
-    private WebElement locationSubway;
-
     //     @Step("Add new location pop-up: select subway or locality")
     public AddLocationComponent selectLocationSubway(String subwayLocality) {
         Select selectFromSubwayLocalityList = new Select(locationSubway);
         selectFromSubwayLocalityList.selectByVisibleText(subwayLocality);
         return this;
     }
-
-    @FindBy(how = How.XPATH, using = ".//*[@id='address']")
-    private WebElement locationAddress;
 
     //     @Step("Add new location pop-up: enter location address")
     public AddLocationComponent enterLocationAddress(String locationAddress) {
@@ -64,9 +66,6 @@ public class AddLocationComponent extends BaseComponent {
         return this;
     }
 
-    @FindBy(how = How.XPATH, using = ".//*[@id='coordinates']")
-    private WebElement locationGC;
-
     //     @Step("Add new location pop-up: enter location geographic coordinates")
     public AddLocationComponent enterLocationGC(String locationGC) {
         this.locationGC.click();
@@ -75,9 +74,6 @@ public class AddLocationComponent extends BaseComponent {
         return this;
     }
 
-    @FindBy(how = How.XPATH, using = ".//*[@id='phone']")
-    private WebElement locationPhone;
-
     //     @Step("Add new location pop-up: enter location phone number")
     public AddLocationComponent enterLocationPhone(String locationPhone) {
         this.locationPhone.click();
@@ -85,9 +81,6 @@ public class AddLocationComponent extends BaseComponent {
         this.locationPhone.sendKeys(locationPhone);
         return this;
     }
-
-    @FindBy(how = How.XPATH, using = ".//*[@type='submit' and contains(@class, 'flooded-button')]")
-    private WebElement addLocationToListButton;
 
     //     @Step("Add new location pop-up: press add new location button to add it to the list of existing locations")
     public AddLocationComponent pressAddLocationToListButton() {
