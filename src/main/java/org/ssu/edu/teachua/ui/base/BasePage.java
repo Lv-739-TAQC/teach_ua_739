34221f3a2c87e50f2883ebfe643baf458eecb1b3
Maksym <86326168+Maksym637@users.noreply.github.com>
2023-02-24 19:57:41 +0200

    Fixing menus functionality + test example (#23)

--- src/main/java/org/ssu/edu/teachua/ui/base/BasePage.java
+++ src/main/java/org/ssu/edu/teachua/ui/base/BasePage.java
@@ -1,19 +1,20 @@
 package org.ssu.edu.teachua.ui.base;
 
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.ssu.edu.teachua.ui.components.header.HeaderComponent;
 
 public abstract class BasePage extends Base {
-    private final HeaderComponent header;
 
-//    @FindBy(how = How.XPATH, using = "//header")
-//    private WebElement headerTitle;
+    @FindBy(how = How.XPATH, using = "//body")
+    private WebElement headerNode;
+
+    private final HeaderComponent header;
 
     public BasePage(WebDriver driver) {
         super(driver);
-        WebElement headerNode = driver.findElement(By.xpath("//header"));
         header = new HeaderComponent(driver, headerNode);
     }
 
--- src/main/java/org/ssu/edu/teachua/ui/components/header/HeaderComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/header/HeaderComponent.java
@@ -7,7 +7,6 @@ import org.openqa.selenium.support.How;
 import org.ssu.edu.teachua.ui.base.BaseComponent;
 import org.ssu.edu.teachua.ui.components.menus.AdminMenuComponent;
 import org.ssu.edu.teachua.ui.components.menus.GuestMenuComponent;
-import org.ssu.edu.teachua.ui.components.menus.MenuComponent;
 import org.ssu.edu.teachua.ui.components.menus.UserMenuComponent;
 import org.ssu.edu.teachua.ui.pages.about.AboutPage;
 import org.ssu.edu.teachua.ui.pages.clubs.ClubsPage;
@@ -15,66 +14,64 @@ import org.ssu.edu.teachua.ui.pages.home.HomePage;
 import org.ssu.edu.teachua.ui.pages.news.NewsPage;
 
 public class HeaderComponent extends BaseComponent {
-    @FindBy(how = How.XPATH, using = "//header")
-    private WebElement profileMenuNode;
+
     @FindBy(how = How.XPATH, using = ".//div[@class='logo']")
     private WebElement logo;
+
     @FindBy(how = How.XPATH, using = ".//span/a[@href='/dev/clubs']")
     private WebElement clubsButton;
+
     @FindBy(how = How.XPATH, using = ".//div[@class='center-side']//a[@href='/dev/news']")
     private WebElement newsButton;
+
     @FindBy(how = How.XPATH, using = ".//div[@class='center-side']//a[@href='/dev/about']")
     private WebElement aboutButton;
-    @FindBy(how = How.XPATH, using = ".//span[@aria-label='user']")
-    private WebElement userProfileButton;
+
+    @FindBy(how = How.XPATH, using = ".//span[contains(@class, 'avatarIfNotLogin')]")
+    private WebElement userIconNotLogin;
+
+    @FindBy(how = How.XPATH, using = ".//span[contains(@class, 'avatarIfLogin')]")
+    private WebElement userIconLogin;
+
+    @FindBy(how = How.XPATH, using = ".//ul[contains(@class, 'ant-dropdown-menu')]")
+    private WebElement profileMenuNode;
 
     public HeaderComponent(WebDriver driver, WebElement node) {
         super(driver, node);
     }
 
     public HomePage clickLogo() {
-        logo.click();
+        waitForElementToBeClickable(logo).click();
         return new HomePage(driver);
     }
 
     public ClubsPage clickClubsButton() {
-        clubsButton.click();
+        waitForElementToBeClickable(clubsButton).click();
         return new ClubsPage(driver);
     }
 
     public NewsPage clickNewsButton() {
-        newsButton.click();
+        waitForElementToBeClickable(newsButton).click();
         return new NewsPage(driver);
     }
 
     public AboutPage clickAboutButton() {
-        aboutButton.click();
+        waitForElementToBeClickable(aboutButton).click();
         return new AboutPage(driver);
     }
 
     public AdminMenuComponent openAdminProfileMenu() {
-        userProfileButton.click();
+        waitForElementToBeClickable(userIconLogin).click();
         return new AdminMenuComponent(driver, profileMenuNode);
     }
 
     public UserMenuComponent openUserProfileMenu() {
-        userProfileButton.click();
+        waitForElementToBeClickable(userIconLogin).click();
         return new UserMenuComponent(driver, profileMenuNode);
     }
 
     public GuestMenuComponent openGuestProfileMenu() {
-        userProfileButton.click();
-        return new GuestMenuComponent(driver, profileMenuNode);
-    }
-
-    public MenuComponent openProfileMenu() {
-        userProfileButton.click();
-        String role = localStorage.getItem("role");
-        if (role.equals("ROLE_ADMIN") || role.equals("ROLE_MANAGER")) {
-            return new AdminMenuComponent(driver, profileMenuNode);
-        } else if (role.equals("ROLE_USER")) {
-            return new UserMenuComponent(driver, profileMenuNode);
-        }
+        waitForElementToBeClickable(userIconNotLogin).click();
         return new GuestMenuComponent(driver, profileMenuNode);
     }
 }
