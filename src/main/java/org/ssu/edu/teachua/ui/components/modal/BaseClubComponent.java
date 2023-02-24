7d18acd8e3962b50a9499ac475e8fd9c4a950d42
TetianaFilatova <t.filatova0564@gmail.com>
2023-02-24 22:41:03 +0200

    add error messages and check/close circle for Add/Edit club

--- src/main/java/org/ssu/edu/teachua/ui/components/modal/BaseClubComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/modal/BaseClubComponent.java
@@ -15,12 +15,23 @@ public abstract class BaseClubComponent extends BaseComponent {
     protected WebElement nextStepButton;
     @FindBy(how = How.XPATH, using = ".//button[contains(@class, 'add-club-content-prev')]")
     protected WebElement previousPageButton;
+    @FindBy(how = How.XPATH, using = ".//span[@class='ant-input-suffix']//span[@aria-label='check-circle']")
+    protected WebElement checkCircle;
+    @FindBy(how = How.XPATH, using = ".//span[@class='ant-input-textarea-suffix']//span[@aria-label='check-circle']")
+    protected WebElement textareaCheckCircle;
+    @FindBy(how = How.XPATH, using = ".//span[@class='ant-input-suffix']//span[@aria-label='close-circle']")
+    protected WebElement closeCircle;
+    @FindBy(how = How.XPATH, using = ".//span[@class='ant-input-textarea-suffix']//span[@aria-label='close-circle']")
+    protected WebElement textareaCloseCircle;
+    @FindBy(how = How.XPATH, using = ".//div[@class='ant-form-item-explain-error']")
+    protected WebElement fieldErrorMsg;
     @FindBy(how = How.XPATH, using = "(.//div[contains(@class, 'item-active')]//div[text()[contains(., 'Основна інформація')]])[1]")
     private WebElement clubMainInfoComponent;
     @FindBy(how = How.XPATH, using = "(.//div[contains(@class, 'item-active')]//div[text()[contains(., 'Контакти')]])[1])")
     private WebElement clubContactsComponent;
     @FindBy(how = How.XPATH, using = "(.//div[contains(@class, 'item-active')]//div[text()[contains(., 'Опис')]])[1]")
     private WebElement clubDescriptionComponent;
+
 
     public BaseClubComponent(WebDriver driver) {
 
