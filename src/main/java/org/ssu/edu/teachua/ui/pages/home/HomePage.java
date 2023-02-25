84fa8b35141e1f52a42c0d851602e11d8e97c477
Denis Pitsul <denia.pitsul@gmail.com>
2023-02-25 17:18:30 +0200

    TUA-510 test implementation

--- src/main/java/org/ssu/edu/teachua/ui/pages/home/HomePage.java
+++ src/main/java/org/ssu/edu/teachua/ui/pages/home/HomePage.java
@@ -1,10 +1,25 @@
 package org.ssu.edu.teachua.ui.pages.home;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
 import org.ssu.edu.teachua.ui.base.BasePage;
+import org.ssu.edu.teachua.ui.components.search.AdvancedSearchClubComponent;
 
 public class HomePage extends BasePage {
+
+    @FindBy(how = How.XPATH, using = ".//span[@title='Розширений пошук']")
+    private WebElement advancedSearchIcon;
+
     public HomePage(WebDriver driver) {
         super(driver);
+    }
+
+    public AdvancedSearchClubComponent clickAdvancedSearchIcon() {
+        waitForElementToAppear(advancedSearchIcon).click();
+        WebElement advancedSearchNode = waitForElementToAppear(driver.findElement(By.xpath("//aside[contains(@class, 'club-list-sider')]")));
+        return new AdvancedSearchClubComponent(driver, advancedSearchNode);
     }
 }
---
+++ src/test/java/org/ssu/edu/teachua/ui/AdvancedSearchTest.java
@@ -0,0 +1,24 @@
+package org.ssu.edu.teachua.ui;
+
+import org.ssu.edu.teachua.ui.components.search.AdvancedSearchCenterComponent;
+import org.ssu.edu.teachua.ui.pages.home.HomePage;
+import org.ssu.edu.teachua.ui.runners.TestRunnerUI;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AdvancedSearchTest extends TestRunnerUI {
+
+    @Test
+    public void verifyThatAvailableOnlineCategoriesChildAgeParametersAreDeactivatedAfterSelectingCenterRadioButton() {
+        AdvancedSearchCenterComponent advancedSearchCenterComponent =new HomePage(driver)
+                .clickAdvancedSearchIcon()
+                .chooseCenter();
+
+//        Assert.assertTrue(advancedSearchCenterComponent.isCityParameterActivated(), "City parameter is deactivated");
+//        Assert.assertTrue(advancedSearchCenterComponent.isDistrictParameterActivated(), "District parameter is deactivated");
+//        Assert.assertTrue(advancedSearchCenterComponent.isStationParameterActivated(), "Station parameter is deactivated");
+        Assert.assertTrue(advancedSearchCenterComponent.isOnlineParameterDeactivated(), "Online parameter is activated");
+        Assert.assertTrue(advancedSearchCenterComponent.isCategoriesParameterDeactivated(), "Categories parameter is activated");
+        Assert.assertTrue(advancedSearchCenterComponent.isChildAgeParameterDeactivated(), "Child age parameter is activated");
+    }
+}
