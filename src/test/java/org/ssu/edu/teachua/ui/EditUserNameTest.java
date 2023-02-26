ed6cab1032fdb919388a9f7e4d446401af310479
Bohdan <bohdan.dzoba1@gmail.com>
2023-02-26 23:07:53 +0200

    implementation for TUA-328

---
+++ src/test/java/org/ssu/edu/teachua/ui/EditUserNameTest.java
@@ -0,0 +1,58 @@
+package org.ssu.edu.teachua.ui;
+
+import org.ssu.edu.teachua.ui.pages.home.HomePage;
+import org.ssu.edu.teachua.ui.runners.TestRunnerUI;
+import org.ssu.edu.teachua.utils.DataProviderTua328;
+import org.ssu.edu.teachua.utils.TestValueProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+public class EditUserNameTest extends TestRunnerUI {
+
+    @Test(dataProvider = "dpTua328", dataProviderClass = DataProviderTua328.class)
+    public void inputWrongNameTest(String insert, String expected) throws IOException {
+
+        TestValueProvider adminCredentials = new TestValueProvider();
+        HomePage homePage = new HomePage(driver);
+
+        homePage.getHeader()
+                .openGuestProfileMenu()
+                .openLogInForm()
+                .enterEmail(adminCredentials.getAdminEmail())
+                .enterPassword(adminCredentials.getAdminPassword())
+                .clickLoginButton();
+
+        String actualResult = homePage.getHeader()
+                .openAdminProfileMenu()
+                .openProfilePage()
+                .clickEditProfileButton()
+                .enterNewFirstName(insert).getAlertMessageFirstName();
+
+        Assert.assertEquals(actualResult, expected);
+    }
+
+    @Test
+    public void deleteNameTest() throws IOException {
+
+        TestValueProvider adminCredentials = new TestValueProvider();
+        HomePage homePage = new HomePage(driver);
+
+        homePage.getHeader()
+                .openGuestProfileMenu()
+                .openLogInForm()
+                .enterEmail(adminCredentials.getAdminEmail())
+                .enterPassword(adminCredentials.getAdminPassword())
+                .clickLoginButton();
+
+        String actualResult = homePage.getHeader()
+                .openAdminProfileMenu()
+                .openProfilePage()
+                .clickEditProfileButton()
+                .enterNewFirstName("normalName")
+                .enterNewFirstName("").getAlertMessageFirstName();
+
+        Assert.assertEquals(actualResult, "Будь ласка введіть Ваше ім'я");
+    }
+}
