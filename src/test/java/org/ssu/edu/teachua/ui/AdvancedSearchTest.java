7b8ce71032380d63a41f8049c5d383561f9c1b99
Denis Pitsul <denia.pitsul@gmail.com>
2023-02-28 14:16:41 +0200

    Use softAssert

--- src/test/java/org/ssu/edu/teachua/ui/AdvancedSearchTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/AdvancedSearchTest.java
@@ -5,17 +5,20 @@ import org.ssu.edu.teachua.ui.pages.home.HomePage;
 import org.ssu.edu.teachua.ui.runners.TestRunnerUI;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class AdvancedSearchTest extends TestRunnerUI {
 
     @Test
     public void verifyThatAvailableOnlineCategoriesChildAgeParametersAreDeactivatedAfterSelectingCenterRadioButton() {
+        SoftAssert softAssert = new SoftAssert();
         AdvancedSearchCenterComponent advancedSearchCenterComponent =new HomePage(driver)
                 .clickAdvancedSearchIcon()
                 .chooseCenter();
 
-        Assert.assertTrue(advancedSearchCenterComponent.isOnlineParameterDeactivated(), "Online parameter is activated");
-        Assert.assertTrue(advancedSearchCenterComponent.isCategoriesParameterDeactivated(), "Categories parameter is activated");
-        Assert.assertTrue(advancedSearchCenterComponent.isChildAgeParameterDeactivated(), "Child age parameter is activated");
+        softAssert.assertTrue(advancedSearchCenterComponent.isOnlineParameterDeactivated(), "Online parameter is activated");
+        softAssert.assertTrue(advancedSearchCenterComponent.isCategoriesParameterDeactivated(), "Categories parameter is activated");
+        softAssert.assertTrue(advancedSearchCenterComponent.isChildAgeParameterDeactivated(), "Child age parameter is activated");
+        softAssert.assertAll();
     }
 }
