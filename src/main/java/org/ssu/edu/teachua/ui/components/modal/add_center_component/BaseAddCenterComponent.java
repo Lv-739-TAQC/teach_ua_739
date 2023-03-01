a73d8dc0f98f863b90150c3dcddf8658cb43b12f
mtvnk <31481739+AnnaMatviienko@users.noreply.github.com>
2023-03-01 13:13:48 +0200

    Fixed small issues in BaseAddCenterComponent (#52)
    * Fixed small issues in BaseAddCenterComponent
    
    * Fixed methods and xPathes for Add center component

--- src/main/java/org/ssu/edu/teachua/ui/components/modal/add_center_component/BaseAddCenterComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/modal/add_center_component/BaseAddCenterComponent.java
@@ -3,10 +3,10 @@ package org.ssu.edu.teachua.ui.components.modal.add_center_component;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.pagefactory.DefaultElementLocatorFactory;
 import org.ssu.edu.teachua.ui.base.BaseComponent;
 
 public abstract class BaseAddCenterComponent extends BaseComponent {
@@ -15,22 +15,29 @@ public abstract class BaseAddCenterComponent extends BaseComponent {
     protected WebElement mainInfo;
     @FindBy(how = How.XPATH, using = "(.//div[contains(@class, 'item-title')])[2]")
     protected WebElement contacts;
+
     @FindBy(how = How.XPATH, using = "(.//div[contains(@class, 'item-title')])[3]")
     protected WebElement description;
     @FindBy(how = How.XPATH, using = "(.//div[contains(@class, 'item-title')])[4]")
     protected WebElement clubs;
-    @FindBy(how = How.XPATH, using = "(.//main[contains(@class, 'add-center-container')]")
+
+    @FindBy(how = How.XPATH, using = ".//main[@class='ant-layout-content add-center-container']")
     protected WebElement addCenterContainer;
+
     @FindBy(how = How.XPATH, using = ".//div[contains(@class, 'ant-modal addCenter')]")
     protected WebElement root;
-
+    @FindBy(how = How.XPATH, using = "(.//div[@class='side-mobile']//span[contains(@class,'ant-steps-icon') and text()=1]//parent::div")
+    protected WebElement firstStep;
+    @FindBy(how = How.XPATH, using = "(.//div[@class='side-mobile']//span[contains(@class,'ant-steps-icon') and text()=2]//parent::div")
+    protected WebElement secondStep;
+    @FindBy(how = How.XPATH, using = "(.//div[@class='side-mobile']//span[contains(@class,'ant-steps-icon') and text()=3]//parent::div")
+    protected WebElement thirdStep;
+    @FindBy(how = How.XPATH, using = "(.//div[@class='side-mobile']//span[contains(@class,'ant-steps-icon') and text()=4]//parent::div")
+    protected WebElement forthStep;
 
     public BaseAddCenterComponent(WebDriver driver) {
         super(driver);
-        componentRoot = driver.findElement(By.xpath(".//div[contains(@class, 'ant-modal addCenter')]"));
-        PageFactory.initElements(new DefaultElementLocatorFactory(componentRoot), this);
     }
-
 
     public WebElement getMainInfo() {
         return mainInfo;
@@ -48,4 +55,27 @@ public abstract class BaseAddCenterComponent extends BaseComponent {
         return clubs;
     }
 
+    public String getFirstStepColor() {
+        String colorCode = firstStep.getCssValue("background-color");
+        String hexacolor = Color.fromString(colorCode).asHex();
+        return hexacolor;
+    }
+
+    public String getSecondStepColor() {
+        String colorCode = secondStep.getCssValue("background-color");
+        String hexacolor = Color.fromString(colorCode).asHex();
+        return hexacolor;
+    }
+
+    public String getThirdStepColor() {
+        String colorCode = thirdStep.getCssValue("background-color");
+        String hexacolor = Color.fromString(colorCode).asHex();
+        return hexacolor;
+    }
+
+    public String getForthStepColor() {
+        String colorCode = forthStep.getCssValue("background-color");
+        String hexacolor = Color.fromString(colorCode).asHex();
+        return hexacolor;
+    }
 }
