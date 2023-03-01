e8fdcc41a0877a48efd9735db2a8272d78d3a3ec
Anna Matviienko <anna.matviienko08@gmail.com>
2023-03-01 13:55:20 +0200

    Created Add location test

---
+++ src/test/java/org/ssu/edu/teachua/ui/AddLocationTest.java
@@ -0,0 +1,31 @@
+package org.ssu.edu.teachua.ui;
+
+import org.ssu.edu.teachua.ui.pages.home.HomePage;
+import org.ssu.edu.teachua.ui.runners.TestRunnerUI;
+import org.testng.annotations.Test;
+
+public class AddLocationTest extends TestRunnerUI {
+
+    @Test
+    public void addLocationTest() {
+        HomePage homePage = new HomePage(driver);
+        homePage.getHeader()
+                .openGuestProfileMenu()
+                .openLogInForm()
+                .enterEmail(valueProvider.getAdminEmail())
+                .enterPassword(valueProvider.getAdminPassword())
+                .clickLoginButton()
+                .getHeader()
+                .openAdminProfileMenu()
+                .openAddCentreForm()
+                .pressAddLocationButton()
+                .enterLocationName("Лівий берег")
+                .selectLocationCity("Одеса")
+                .selectLocationSubway("Фонтан")
+                .selectLocationDistrict("Приморський")
+                .enterLocationAddress("проспект Бажана, 3А")
+                .enterLocationGC("50.406108, 30.668492")
+                .enterLocationPhone("0679002233")
+                .pressAddLocationToListButton();
+    }
+}
