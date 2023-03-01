ba5a3242a50e1ff75d0913ce4307e6d4bfd75abd
Kapustin Ilya <63232432+kapustin-ilya@users.noreply.github.com>
2023-03-01 13:19:11 +0200

    Update advanced search componenet/kapustin illia (#53)
    * Updated AdvancedSearchCenterComponent
    
    * Updated ClubCardComponent

--- src/main/java/org/ssu/edu/teachua/ui/components/card/ClubCardComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/card/ClubCardComponent.java
@@ -1,5 +1,7 @@
 package org.ssu.edu.teachua.ui.components.card;
 
+import java.util.List;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
@@ -9,22 +11,36 @@ import org.ssu.edu.teachua.ui.pages.view.ViewClubPage;
 
 public class ClubCardComponent extends BaseComponent {
 
-    @FindBy(how = How.XPATH, using = ".//div[@class='name']")
-    private WebElement clubTitle;
+	@FindBy(how = How.XPATH, using = ".//div[@class='name']")
+	private WebElement clubTitle;
 
-    @FindBy(how = How.XPATH, using = ".//button[contains(@class, 'details-button')]")
-    private WebElement detailsButton;
+	@FindBy(how = How.XPATH, using = ".//button[contains(@class, 'details-button')]")
+	private WebElement detailsButton;
 
-    public ClubCardComponent(WebDriver driver, WebElement node) {
-        super(driver, node);
-    }
+	@FindBy(how = How.XPATH, using = ".//li[contains(@class , 'ant-rate-star')]")
+	private List<WebElement> starsRating;
 
-    public String getClubTitle() {
-        return clubTitle.getText();
-    }
+	public ClubCardComponent(WebDriver driver, WebElement node) {
+		super(driver, node);
+	}
 
-    public ViewClubPage clickDetailsButton() {
-        detailsButton.click();
-        return new ViewClubPage(driver);
-    }
+	public String getClubTitle() {
+		return clubTitle.getText();
+	}
+
+	public ViewClubPage clickDetailsButton() {
+		detailsButton.click();
+		return new ViewClubPage(driver);
+	}
+
+	public int getRating() {
+		int count = 0;
+		for (WebElement star : starsRating) {
+			if (star.getAttribute("class").contains("ant-rate-star-full")){
+				count++;
+			}
+		}
+		return count;
+	}
+
 }
