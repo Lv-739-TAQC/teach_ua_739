9f041610cfa5f68b080040bccbe2cc9f830295e6
mtvnk <31481739+AnnaMatviienko@users.noreply.github.com>
2023-03-02 15:43:15 +0200

    Created Add location test (#63)
    * Created Add location test
    
    * Implementation of TUA-158 Add location

---
+++ src/test/java/org/ssu/edu/teachua/ui/AddLocationTest.java
@@ -0,0 +1,34 @@
+package org.ssu.edu.teachua.ui;
+
+import org.ssu.edu.teachua.ui.pages.home.HomePage;
+import org.ssu.edu.teachua.ui.runners.TestRunnerUI;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class AddLocationTest extends TestRunnerUI {
+
+    @Test
+    public void addLocationTest() {
+        HomePage homePage = new HomePage(driver);
+        String actualName = homePage.getHeader()
+                .openGuestProfileMenu()
+                .openLogInForm()
+                .enterEmail(valueProvider.getAdminEmail())
+                .enterPassword(valueProvider.getAdminPassword())
+                .clickLoginButton()
+                .getHeader()
+                .openAdminProfileMenu()
+                .openAddCentreForm()
+                .pressAddLocationButton()
+                .enterLocationName("Лівий берег")
+                .selectLocationCity("Одеса")
+                .selectLocationSubway("Фонтан")
+                .selectLocationDistrict("Приморський")
+                .enterLocationAddress("проспект Бажана, 3А")
+                .enterLocationGC("50.406108, 30.668492")
+                .enterLocationPhone("0679002233")
+                .pressAddLocationToListButton()
+                .getNameNewLocation();
+        Assert.assertEquals(actualName, "Лівий берег");
+    }
+}
