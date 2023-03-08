c7de386e8b0ab8caa477477f1bf71b8d6e2067da
Zhanna Stadnytska <janelischuk@gmail.com>
2023-03-08 15:57:49 +0200

    open profile page precondition

--- src/test/java/org/ssu/edu/teachua/ui/profile/ProfilePageTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/profile/ProfilePageTest.java
@@ -1,6 +1,17 @@
 package org.ssu.edu.teachua.ui.profile;
 
+import org.ssu.edu.teachua.ui.pages.home.HomePage;
 import org.ssu.edu.teachua.ui.runners.LoginWithAdminRunner;
+import org.testng.annotations.BeforeTest;
 
 public class ProfilePageTest extends LoginWithAdminRunner {
+
+    @BeforeTest(description = "Precondition method : open Profile Page")
+    public void openProfilePagePrecondition() {
+        HomePage homePage = new HomePage(driver);
+
+        homePage.getHeader()
+                .openAdminProfileMenu()
+                .openProfilePage();
+    }
 }
