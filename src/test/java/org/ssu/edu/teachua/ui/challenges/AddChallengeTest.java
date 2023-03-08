aed12a42901c89c69ee3d8e531e29de377cef532
Zhanna Stadnytska <janelischuk@gmail.com>
2023-03-09 00:41:51 +0200

    added two addChallenge tests, dataprovider

--- src/test/java/org/ssu/edu/teachua/ui/challenges/AddChallengeTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/challenges/AddChallengeTest.java
@@ -3,8 +3,12 @@ package org.ssu.edu.teachua.ui.challenges;
 import org.ssu.edu.teachua.ui.pages.challenges.AddChallengePage;
 import org.ssu.edu.teachua.ui.pages.home.HomePage;
 import org.ssu.edu.teachua.ui.runners.LoginWithAdminRunner;
+import org.ssu.edu.teachua.utils.providers.DataProviderChallenge;
+import org.testng.Assert;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
+
+import java.util.List;
 
 public class AddChallengeTest extends LoginWithAdminRunner {
 
@@ -43,6 +47,58 @@ public class AddChallengeTest extends LoginWithAdminRunner {
 
         softAssert.assertEquals(actualSuccessMsg, expectedSuccessMsg);
         softAssert.assertEquals(actualTitle, expectedTitle);
+
+        softAssert.assertAll();
+    }
+
+    @Test(dataProvider = "dpTestCompressAndUploadPhoto", dataProviderClass = DataProviderChallenge.class)
+    public void verifyIfPhotoCompressedAndUploadedTest(String photoPath) {
+
+        addChallengePage.addPhoto(valueProvider.getFilePath(photoPath));
+
+        Assert.assertTrue(addChallengePage.getPhotoAppeared().isDisplayed());
+    }
+
+    @Test(dataProvider = "dpTestErrorMessageChallengeNameField", dataProviderClass = DataProviderChallenge.class)
+    public void verifyErrorMessagesForChallengeNameFieldTest(String sortNumber, String title, String description, String photoPath, List<String> invalidNames, String redBorderColor, List<String> expectedErrorMsg) {
+
+        String actualErrorFirst = addChallengePage
+                .fillSortNumber(sortNumber)
+                .fillTitle(title)
+                .fillDescription(description)
+                .addPhoto(valueProvider.getFilePath(photoPath))
+                .fillName(invalidNames.get(0))
+                .clickSave()
+                .checkErrorMessage();
+        softAssert.assertEquals(actualErrorFirst, expectedErrorMsg.get(0));
+
+        String actualErrorSecond = addChallengePage
+                .waitForErrorMessageToDisappear()
+                .clearName()
+                .fillName(invalidNames.get(1))
+                .clickSave()
+                .checkErrorMessage();
+        softAssert.assertEquals(actualErrorSecond, expectedErrorMsg.get(1));
+
+        String actualErrorThird = addChallengePage
+                .waitForErrorMessageToDisappear()
+                .clearName()
+                .fillName(invalidNames.get(2))
+                .clickSave()
+                .checkErrorMessage();
+        softAssert.assertEquals(actualErrorThird, expectedErrorMsg.get(2));
+
+        String actualErrorFourth = addChallengePage
+                .waitForErrorMessageToDisappear()
+                .clearName()
+                .fillName(invalidNames.get(3))
+                .clickSave()
+                .checkErrorMessage();
+        softAssert.assertEquals(actualErrorFourth, expectedErrorMsg.get(3));
+
+        String actualBorderColor = addChallengePage
+                .getBorderColorForNameField();
+        softAssert.assertEquals(actualBorderColor, redBorderColor);
 
         softAssert.assertAll();
     }
--- src/test/java/org/ssu/edu/teachua/utils/providers/DataProviderChallenge.java
+++ src/test/java/org/ssu/edu/teachua/utils/providers/DataProviderChallenge.java
@@ -2,6 +2,8 @@ package org.ssu.edu.teachua.utils.providers;
 
 import org.testng.annotations.DataProvider;
 
+import java.util.Arrays;
+
 public class DataProviderChallenge {
     @DataProvider(name = "dpTestErrorMessageChallengeNameField")
     public Object[][] dpTestErrorMessageChallengeNameField() {
@@ -10,12 +12,12 @@ public class DataProviderChallenge {
                         "testFeb2023",
                         "Lorem Ipsum is simply dummy text of the printing and typesetting industry.",
                         "photos/image.png",
-                        new String[]{"Дыผð*.:", "2", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", ""},
+                        Arrays.asList("Дыผð*.:", "2", "Lorem Ipsum is simply dummy text of the printing and typesetting industry.", ""),
                         "rgb(255, 0, 0)",
-                        new String[]{"Це поле може містити тільки українські та англійські літери, цифри та спеціальні символи",
+                        Arrays.asList("Це поле може містити тільки українські та англійські літери, цифри та спеціальні символи",
                                 "Назва Челенджу закоротка",
                                 "Назва Челенджу задовга",
-                                "Поле не повинно бути пустим"}
+                                "Поле не повинно бути пустим")
                 }
         };
     }
