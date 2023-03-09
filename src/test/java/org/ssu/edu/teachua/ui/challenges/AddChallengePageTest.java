9e12989396832dd094ae8352741f494ba9647dd4
Ira Zhurakivska <122312322+IrenZh@users.noreply.github.com>
2023-03-09 12:48:32 +0200

    Added before method to add challenge test (#80)
    * Added before method to add challenge test
    
    * rename

--- src/test/java/org/ssu/edu/teachua/ui/challenges/AddChallengePageTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/challenges/AddChallengePageTest.java
@@ -3,11 +3,12 @@ package org.ssu.edu.teachua.ui.challenges;
 import org.ssu.edu.teachua.ui.pages.challenges.AddChallengePage;
 import org.ssu.edu.teachua.ui.pages.home.HomePage;
 import org.ssu.edu.teachua.ui.runners.LoginWithAdminRunner;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
-public class AddChallengeTest extends LoginWithAdminRunner {
-
+public class AddChallengePageTest extends LoginWithAdminRunner {
+    private AddChallengePage addChallengePage;
     @DataProvider(name = "dpTestAddChallengeValid")
     public Object[][] dpTestAddChallengeValid() {
         return new Object[][]{
@@ -16,18 +17,24 @@ public class AddChallengeTest extends LoginWithAdminRunner {
         };
     }
 
-    @Test(dataProvider = "dpTestAddChallengeValid")
-    public void testAddChallengeValid(String number, String name, String title,
-                                      String description, String photoName,
-                                      String expectedSuccessMsg, String expectedTitle) {
-        HomePage homePage = new HomePage(driver);
-
-        String actualSuccessMsg = homePage.getHeader()
+    @BeforeMethod
+    void openAddChallengePage() {
+        driver.navigate().refresh();
+        addChallengePage = new HomePage(driver)
+                .getHeader()
                 .openAdminProfileMenu()
                 .openContentMenu()
                 .openChallengesMenu()
                 .clickChallenges()
-                .addChallenge()
+                .addChallenge();
+    }
+
+    @Test(dataProvider = "dpTestAddChallengeValid")
+    public void testAddChallengeValid(String number, String name, String title,
+                                      String description, String photoName,
+                                      String expectedSuccessMsg, String expectedTitle) {
+
+        String actualSuccessMsg = addChallengePage
                 .fillSortNumber(number)
                 .fillName(name)
                 .fillTitle(title)
@@ -46,4 +53,5 @@ public class AddChallengeTest extends LoginWithAdminRunner {
 
         softAssert.assertAll();
     }
+
 }
