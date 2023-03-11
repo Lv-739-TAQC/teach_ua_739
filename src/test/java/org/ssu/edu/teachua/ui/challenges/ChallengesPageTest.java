8fcc1e83a72d47a74da5bea64bbd55ddd7da0bd4
Zhanna Stadnytska <58187708+janestadnytska@users.noreply.github.com>
2023-03-12 00:26:54 +0200

    added @description into tests (#98)
    * added @step & @issue annotation
    
    * refactor of @step comments
    
    * added @description into tests

--- src/test/java/org/ssu/edu/teachua/ui/challenges/ChallengesPageTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/challenges/ChallengesPageTest.java
@@ -60,6 +60,7 @@ public class ChallengesPageTest extends LoginWithAdminRunner {
     }
 
     @Issue("TUA-229")
+    @Description("Verify that a 'Керівник' can add a photo with invalid parameters\n into the ‘Баннер’ file picker")
     @Test(dataProvider = "dpTestCompressAndUploadPhoto", dataProviderClass = DataProviderChallenge.class)
     public void testIfPhotoCompressedAndUploaded(String photoPath) {
         addChallengePage.addPhoto(valueProvider.getFilePath(photoPath));
@@ -67,6 +68,7 @@ public class ChallengesPageTest extends LoginWithAdminRunner {
     }
 
     @Issue("TUA-231")
+    @Description("Verify the error messages of ‘Назва Челенджу’ field\n on ‘Основна інформація’ tab of 'Додати челендж' pop-up")
     @Test(dataProvider = "dpTestErrorMessageChallengeNameField", dataProviderClass = DataProviderChallenge.class)
     public void testErrorMessagesForChallengeNameField(String sortNumber, String title, String description,
                                                        String photoPath, List<String> invalidNames,
