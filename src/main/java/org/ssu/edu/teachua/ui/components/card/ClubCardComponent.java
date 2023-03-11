1ec06665ab7deb95bfc34e8f1b4c41250b44d923
kapustinILya <k.ilya.v@gmail.com>
2023-03-11 19:59:38 +0200

    Moved Tua103 to AdvancedSearchPageTest

--- src/main/java/org/ssu/edu/teachua/ui/components/card/ClubCardComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/card/ClubCardComponent.java
@@ -13,6 +13,9 @@ public class ClubCardComponent extends BaseComponent {
 
     @FindBy(how = How.XPATH, using = ".//div[@class='name']")
     private WebElement clubTitle;
+    
+    @FindBy(how = How.XPATH, using = ".//div[@class='center-name']")
+    private WebElement centerTitle;
 
     @FindBy(how = How.XPATH, using = ".//button[contains(@class, 'details-button')]")
     private WebElement detailsButton;
@@ -26,6 +29,10 @@ public class ClubCardComponent extends BaseComponent {
 
     public String getClubTitle() {
         return clubTitle.getText();
+    }
+    
+    public String getCenterTitle() {
+        return centerTitle.getText();
     }
 
     public ViewClubPage clickDetailsButton() {
