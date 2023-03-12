28cb8ab91878925543714fe8b7237c700532d750
IraZhurakivska <zhurakivskairen@ukr.net>
2023-03-12 16:40:21 +0200

    fix

--- src/main/java/org/ssu/edu/teachua/ui/components/search/AdvancedSearchClubComponent.java
+++ src/main/java/org/ssu/edu/teachua/ui/components/search/AdvancedSearchClubComponent.java
@@ -6,7 +6,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.ssu.edu.teachua.ui.pages.challenges.AddChallengePage;
 
 public class AdvancedSearchClubComponent extends AdvancedSearchCenterComponent {
 
