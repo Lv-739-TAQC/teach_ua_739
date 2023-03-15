1a1ad0afa858f02599c684092012e6391f592b82
Janet <janet.tafiy@gmail.com>
2023-03-16 00:17:07 +0200

    Added cities' column names and getters()

---
+++ src/main/java/org/ssu/edu/teachua/db/entities/City.java
@@ -0,0 +1,30 @@
+package org.ssu.edu.teachua.db.entities;
+
+import lombok.Data;
+import org.ssu.edu.teachua.db.annotation.Column;
+import org.ssu.edu.teachua.db.annotation.TableDB;
+
+@TableDB(name = "cities")
+@Data
+public class City extends Entity {
+
+    @Column(name = "latitude")
+    private Double latitude;
+    @Column(name = "longitude")
+    private Double longitude;
+    @Column(name = "name")
+    private String name;
+
+    public Double getLatitude() {
+        return latitude;
+    }
+
+    public Double getLongitude() {
+        return longitude;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
