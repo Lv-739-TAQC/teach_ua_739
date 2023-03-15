f35b8f37b775a9cf1b67b4252b1656fd7cdd2bb6
TetianaFilatova <122798739+TetianaFilatova@users.noreply.github.com>
2023-03-15 13:31:41 +0200

    multi browser testing implementation (#112)
    * multi browser testing implementation

--- src/test/java/org/ssu/edu/teachua/utils/runners/BaseTestRunnerUI.java
+++ src/test/java/org/ssu/edu/teachua/utils/runners/BaseTestRunnerUI.java
@@ -3,22 +3,35 @@ package org.ssu.edu.teachua.utils.runners;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.ssu.edu.teachua.utils.TestNgListeners;
+import org.openqa.selenium.support.FindBy;
 import org.ssu.edu.teachua.utils.TestValueProvider;
+import org.ssu.edu.teachua.utils.runners.Browsers;
 import org.testng.ITestContext;
 import org.testng.annotations.*;
 import org.testng.asserts.SoftAssert;
 
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
 
-@Listeners(TestNgListeners.class)
 public class BaseTestRunnerUI {
 
     protected static TestValueProvider valueProvider;
     protected WebDriver driver;
     protected SoftAssert softAssert = new SoftAssert();
+    Browsers browsers = new Browsers();
+
+    private void checkErrorPage(String browser) {
+        try {
+            driver.findElement(By.id(browser.equals("firefox") ? "advancedButton" : "details-button")).click();
+            driver.findElement(By.id(browser.equals("firefox") ? "exceptionDialogButton" : "proceed-link")).click();
+        } catch (RuntimeException exception) {
+            System.out.println(exception.getMessage());
+        }
+    }
 
     @BeforeSuite
     public void initTestValueProvider() {
@@ -28,29 +41,19 @@ public class BaseTestRunnerUI {
         WebDriverManager.chromedriver().setup();
     }
 
+    @Parameters("browser")
     @BeforeClass(description = "Init ChromeDriver.")
-    protected void initDriver(ITestContext context) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
-//        options.addArguments("--headless");
-        driver = new ChromeDriver(options);
-        context.setAttribute("myDriver", driver);
+    protected void initDriver(String browser, ITestContext context) throws InterruptedException {
+        driver = browsers.setUpBrowser(browser);
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.get(valueProvider.getBaseUiUrl());
-        driver.findElement(By.id("details-button")).click();
-        driver.findElement(By.id("proceed-link")).click();
+        checkErrorPage(browser);
     }
 
+    @Parameters("browser")
     @AfterClass
     public void closeDriver() {
-        if (driver != null) {
-            driver.close();
-        }
-    }
-
-    @AfterSuite
-    public void quitDriver() {
         if (driver != null) {
             driver.quit();
         }
