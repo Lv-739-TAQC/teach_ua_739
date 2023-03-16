c0ba8bf0d6bfaff64c2cd32aea6874b4ba67e458
Zhanna Stadnytska <janelischuk@gmail.com>
2023-03-16 17:15:17 +0200

    added CityService

---
+++ src/main/java/org/ssu/edu/teachua/db/service/CityService.java
@@ -0,0 +1,24 @@
+package org.ssu.edu.teachua.db.service;
+
+import org.ssu.edu.teachua.db.entities.City;
+import org.ssu.edu.teachua.db.repository.DBException;
+import org.ssu.edu.teachua.db.repository.DBManager;
+import org.ssu.edu.teachua.db.repository.EntityException;
+import org.ssu.edu.teachua.db.repository.impl.CityDAOImpl;
+
+import java.util.List;
+
+public class CityService {
+
+    static final String SQL_FIND_CITY_BY_NAME = "SELECT * FROM cities WHERE name = ?;";
+
+    public static List<City> getCities() throws DBException, EntityException {
+        return new CityDAOImpl().findAll(DBManager.getInstance().getConnection(), false);
+    }
+
+    public static List<City> getCitiesByName(String name) throws DBException, EntityException {
+        return new CityDAOImpl().findElementsBySQlRequest(
+                DBManager.getInstance().getConnection(), SQL_FIND_CITY_BY_NAME, true, name
+        );
+    }
+}
