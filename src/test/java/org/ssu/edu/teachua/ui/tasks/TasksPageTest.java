1dad93635c76b64249494de252e44be6097b83b1
mtvnk <31481739+AnnaMatviienko@users.noreply.github.com>
2023-03-17 21:42:22 +0200

    Implemented methods checking if add tasks fields are empty (#127)

--- src/test/java/org/ssu/edu/teachua/ui/tasks/TasksPageTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/tasks/TasksPageTest.java
@@ -1,6 +1,29 @@
 package org.ssu.edu.teachua.ui.tasks;
 
+import org.ssu.edu.teachua.ui.pages.home.HomePage;
+import org.ssu.edu.teachua.ui.pages.tasks.AddTaskPage;
 import org.ssu.edu.teachua.utils.runners.LoginWithAdminRunner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
 
 public class TasksPageTest extends LoginWithAdminRunner {
+    private AddTaskPage addTaskPage;
+
+    @BeforeMethod
+    void openAddTaskPage() {
+        driver.navigate().refresh();
+        addTaskPage = new HomePage(driver)
+                .getHeader()
+                .openAdminProfileMenu()
+                .openContentMenu()
+                .openChallengesMenu()
+                .clickTasks()
+                .openAddTaskPage();
+    }
+
+    @Test
+    public void testDescriptionFieldInvalid() {
+        System.out.println("Result is: " + addTaskPage.areWebElementsEmpty());
+    }
+
 }
