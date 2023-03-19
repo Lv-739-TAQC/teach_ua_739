e8cde10a9d1e9ad8790c140e4ceef39a18b7e14d
Anna Matviienko <anna.matviienko08@gmail.com>
2023-03-19 23:04:38 +0200

    Implemented TUA-525 DB test

--- src/test/java/org/ssu/edu/teachua/ui/tasks/TasksPageTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/tasks/TasksPageTest.java
@@ -1,10 +1,20 @@
 package org.ssu.edu.teachua.ui.tasks;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import org.ssu.edu.teachua.db.entities.Task;
+import org.ssu.edu.teachua.db.repository.DBException;
+import org.ssu.edu.teachua.db.repository.EntityException;
+import org.ssu.edu.teachua.db.service.TaskService;
 import org.ssu.edu.teachua.ui.pages.home.HomePage;
 import org.ssu.edu.teachua.ui.pages.tasks.AddTaskPage;
+import org.ssu.edu.teachua.utils.providers.DataProviderTask;
 import org.ssu.edu.teachua.utils.runners.LoginWithAdminRunner;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import java.util.List;
 
 public class TasksPageTest extends LoginWithAdminRunner {
     private AddTaskPage addTaskPage;
@@ -24,6 +34,40 @@ public class TasksPageTest extends LoginWithAdminRunner {
     @Test
     public void testDescriptionFieldInvalid() {
         System.out.println("Result is: " + addTaskPage.areWebElementsEmpty());
+    }
+
+    @Issue("TUA-525")
+    @Description("This test-case covers negative scenario when introducing changes" +
+            "\n to the task's 'Опис' field results in error message shown")
+    @Test(dataProvider = "dpTestAddTaskInvalidDescription", dataProviderClass = DataProviderTask.class)
+    public void testAddTaskInvalidDescription(int day, int month, int year, String photoPath, String name,
+                                              String title, String description, String challenge,
+                                              String expectedErrorMessage) {
+        SoftAssert softAssert = new SoftAssert();
+        softAssert.assertTrue(addTaskPage.areWebElementsEmpty());
+
+        String actualErrorMessage = addTaskPage
+                .selectStartDate(day, month, year)
+                .uploadPhoto(valueProvider.getFilePath(photoPath))
+                .typeName(name)
+                .typeTitle(title)
+                .typeDescription(description)
+                .selectChallenge(challenge)
+                .clickSaveButton()
+                .getErrorMsg();
+
+        softAssert.assertEquals(actualErrorMessage, expectedErrorMessage);
+
+        try {
+            TaskService taskService = new TaskService(valueProvider.getDbUrl(), valueProvider.getDbUserName(),
+                    valueProvider.getUDbUserPassword());
+            List<Task> list = taskService.getTasksByName(name);
+            softAssert.assertEquals(list, 0);
+        } catch (DBException | EntityException e) {
+            e.printStackTrace();
+        }
+
+        softAssert.assertAll();
     }
 
 }
--- src/test/java/org/ssu/edu/teachua/utils/providers/DataProviderTask.java
+++ src/test/java/org/ssu/edu/teachua/utils/providers/DataProviderTask.java
@@ -1,4 +1,34 @@
 package org.ssu.edu.teachua.utils.providers;
 
+import org.testng.annotations.DataProvider;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class DataProviderTask {
+
+    public static final List<String> ERROR_MSG = Arrays.asList(
+            "Поле 'Опис' не може бути пустим",
+            "description Помилка. Текст містить недопустимі символи",
+            "Поле 'Опис' може містити мінімум 40 максимум 3000 символів",
+            "Поле 'Опис' може містити мінімум 40 максимум 3000 символів"
+    );
+
+    @DataProvider(name = "dpTestAddTaskInvalidDescription")
+    public static Object[][] dpTestAddTask() {
+        return new Object[][]{
+                {25, 05, 2025, "photos/image.png", "Впровадження електронного каталогу",
+                        "Електронний каталог: досвід впровадження та використання",
+                        "", "Ukrainian", ERROR_MSG.get(0)},
+                {25, 05, 2025, "photos/image.png", "Впровадження електронного каталогу",
+                        "Електронний каталог: досвід впровадження та використання",
+                        "ъэы, ผม, Ÿ, ðъэы, ผม, Ÿ, ðъэы, ผม, Ÿ, ðъэы, ผม, Ÿ, ð", "Ukrainian", ERROR_MSG.get(1)},
+                {25, 05, 2025, "photos/image.png", "Впровадження електронного каталогу",
+                        "Електронний каталог: досвід впровадження та використання",
+                        "Description field input symbols length.", "Ukrainian", ERROR_MSG.get(2)},
+                {25, 05, 2025, "photos/image.png", "Впровадження електронного каталогу",
+                        "Електронний каталог: досвід впровадження та використання",
+                        ("descriptio").repeat(30) + ".", "Ukrainian", ERROR_MSG.get(3)}
+        };
+    }
 }
