5e68dd3e7493c66701aca2f7351335b7aacbb22d
Denis Pitsul <denia.pitsul@gmail.com>
2023-03-20 14:58:02 +0200

    add java doc for task pages

--- src/main/java/org/ssu/edu/teachua/ui/pages/tasks/AddTaskPage.java
+++ src/main/java/org/ssu/edu/teachua/ui/pages/tasks/AddTaskPage.java
@@ -49,6 +49,13 @@ public class AddTaskPage extends BasePage {
         super(driver);
     }
 
+    /**
+     * select date for future task
+     * @param day - day of task
+     * @param month - month of task
+     * @param year - year of task
+     * @return AddTaskPage with selected date
+     */
     @Step("Select date with '{day}' day, '{month}' month and '{year}' year into task start date field")
     public AddTaskPage selectStartDate(int day, int month, int year) {
         waitForElementToAppear(startDateInput).click();
@@ -57,30 +64,55 @@ public class AddTaskPage extends BasePage {
         return this;
     }
 
+    /**
+     * upload photo fo task
+     * @param photoPath - photoPath from computer
+     * @return AddTaskPage with selected photo
+     */
     @Step("Upload photo from '{photoPath}' into task photo field")
     public AddTaskPage uploadPhoto(String photoPath) {
         waitForElementToAppear(photoInput).sendKeys(photoPath);
         return this;
     }
 
+    /**
+     * type name for task
+     * @param name - name of the task
+     * @return AddTaskPage with typed name
+     */
     @Step("Type '{name}' into task name field")
     public AddTaskPage typeName(String name) {
         waitForElementToAppear(nameInput).sendKeys(name);
         return this;
     }
 
+    /**
+     * type title for task
+     * @param title - title of the task
+     * @return AddTaskPage with typed title
+     */
     @Step("Type '{title}' into task title field")
     public AddTaskPage typeTitle(String title) {
         waitForElementToAppear(titleInput).sendKeys(title);
         return this;
     }
 
+    /**
+     * type description for task
+     * @param description - description of the task
+     * @return AddTaskPage with typed description
+     */
     @Step("Type '{description}' into task description field")
     public AddTaskPage typeDescription(String description) {
         waitForElementToAppear(descriptionInput).sendKeys(description);
         return this;
     }
 
+    /**
+     * select challenge from dropdown list for task
+     * @param challenge - challenge name of the task
+     * @return AddTaskPage with selected challenge
+     */
     @Step("Select '{challenge}' into task from challenge dropdown")
     public AddTaskPage selectChallenge(String challenge) {
         waitForElementToAppear(challengeDropdown).click();
@@ -88,28 +120,48 @@ public class AddTaskPage extends BasePage {
         return this;
     }
 
+    /**
+     * click save button and create task
+     * @return ViewChallengePage with created task
+     */
     @Step("Click save button and return ViewTaskPage")
     public ViewChallengePage clickSuccessSaveButton() {
         waitForElementToAppear(saveBtn).click();
         return new ViewChallengePage(driver);
     }
+
+    /**
+     * click save button with invalid data to show error message
+     * @return AddTaskPage with error message
+     */
     @Step("Click save button and return AddTaskPage")
     public AddTaskPage clickFailSaveButton() {
         waitForElementToAppear(saveBtn).click();
         return this;
     }
 
+    /**
+     * show success message of created task
+     * @return text of success message
+     */
     @Step("Get success message")
     public String checkSuccessMessage() {
         return waitForElementToAppear(successMessage).getText();
     }
 
+    /**
+     * show error message of uncreated task
+     * @return text of error message
+     */
     @Step("Get error message")
     public String checkErrorMessage() {
         return waitForElementToAppear(errorMessage).getText();
     }
 
-
+    /**
+     * Check if same input parameter are empty
+     * @return boolean value if same input parameter are empty
+     */
     public boolean areWebElementsEmpty() {
         List<WebElement> taskPageFieldsList = new ArrayList<WebElement>(Arrays.asList(
                 startDateInput, photoInput, nameInput, titleInput, descriptionInput, challengeDropdown
