4a9dc60ccded29e24a4b350cebad717f429b78ff
Denis Pitsul <denia.pitsul@gmail.com>
2023-03-21 13:20:55 +0200

    update generateRandomString

--- src/test/java/org/ssu/edu/teachua/utils/StringGenerator.java
+++ src/test/java/org/ssu/edu/teachua/utils/StringGenerator.java
@@ -5,8 +5,6 @@ import org.apache.commons.lang3.RandomStringUtils;
 public class StringGenerator {
 
     public static String generateRandomString(int stringLength) {
-        boolean useLetters = true;
-        boolean useNumbers = false;
-        return RandomStringUtils.random(stringLength, useLetters, useNumbers);
+        return RandomStringUtils.random(stringLength, true, false);
     }
 }
