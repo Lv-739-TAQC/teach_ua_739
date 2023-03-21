92590399cdad950b046507c65044a69eb2bfba32
Zhanna Stadnytska <58187708+janestadnytska@users.noreply.github.com>
2023-03-21 14:09:29 +0200

    added getClubService into EntityService class & updated tests ; +javadoc (#135)
    * added getClubService into EntityService class & updated tests
    
    * added javadoc into my pages/components
    
    * added javadoc comments for classes & packages
    
    * added limit 6 into query
    
    * changes test

--- src/main/java/org/ssu/edu/teachua/db/service/ClubService.java
+++ src/main/java/org/ssu/edu/teachua/db/service/ClubService.java
@@ -13,8 +13,8 @@ public class ClubService extends BaseService {
     static final String SQL_FIND_CLUB_BY_NAME = "SELECT * FROM clubs WHERE name = ?;";
     static final String SQL_FIND_CLUB_BY_CITY = "SELECT * FROM clubs as cl JOIN locations l ON cl.id = l.club_id JOIN cities ct ON l.city_id = ct.id WHERE ct.name = ?;";
     static final String SQL_FIND_CLUB_BY_PREFIX = "SELECT * FROM clubs WHERE name like ?;";
-    static final String SQL_FIND_CLUB_SORTED_BY_RATING_ASC = "SELECT * FROM clubs WHERE id IN (SELECT DISTINCT club_category.club_id FROM club_category) ORDER BY rating ASC, id;";
-    static final String SQL_FIND_CLUB_SORTED_BY_RATING_DESC = "SELECT * FROM clubs WHERE id IN (SELECT DISTINCT club_category.club_id FROM club_category) ORDER BY rating DESC, id;";
+    static final String SQL_FIND_CLUB_SORTED_BY_RATING_ASC = "SELECT * FROM clubs WHERE id IN (SELECT DISTINCT club_category.club_id FROM club_category) ORDER BY rating ASC, id LIMIT 6;";
+    static final String SQL_FIND_CLUB_SORTED_BY_RATING_DESC = "SELECT * FROM clubs WHERE id IN (SELECT DISTINCT club_category.club_id FROM club_category) ORDER BY rating DESC, id LIMIT 6;";
 
 
     public ClubService(String url, String username, String password) throws DBException {
