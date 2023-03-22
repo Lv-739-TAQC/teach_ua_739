0b4aac144beed79215aaefd81f579bfa46072b30
Bohdan <bohdan.dzoba1@gmail.com>
2023-03-22 21:10:19 +0200

    some fixes

--- src/test/java/org/ssu/edu/teachua/ui/club/ClubModalAsAdminTest.java
+++ src/test/java/org/ssu/edu/teachua/ui/club/ClubModalAsAdminTest.java
@@ -84,9 +84,9 @@ public class ClubModalAsAdminTest extends LoginWithAdminRunner {
     @Severity(SeverityLevel.CRITICAL)
     @Test(dataProvider = "dpTestAddLocationForClub", dataProviderClass = DataProviderClub.class)
     public void testAddLocationForClub(String nameField, int categoryNum, String childAgeFrom, String childAgeFor,
-                                       String locationNameField, String cityField, String districtField, String subwayField,
-                                       String addressField, String coordinatesField, String phoneField, String contactPhone,
-                                       String description) {
+                                       String locationNameField, String cityField, String districtField, String addressField,
+                                       String coordinatesField, String phoneField, String contactPhone, String description,
+                                       String expectedResult) {
 
         mainInfoComponent.enterClubName(nameField)
                 .getCategoriesCheckBoxes(categoryNum)
@@ -97,7 +97,6 @@ public class ClubModalAsAdminTest extends LoginWithAdminRunner {
                 .enterLocationName(locationNameField)
                 .selectLocationCity(cityField)
                 .selectLocationDistrict(districtField)
-                .selectLocationSubway(subwayField)
                 .enterLocationAddress(addressField)
                 .enterLocationGC(coordinatesField)
                 .enterLocationPhone(phoneField)
@@ -111,15 +110,7 @@ public class ClubModalAsAdminTest extends LoginWithAdminRunner {
                 .clickEndButton();
 
         Location location = entityService.getLocationService().getLocationByName(locationNameField).get(0);
-        SoftAssert softAssert = new SoftAssert();
-        softAssert.assertEquals(
-                Arrays.asList(location.getName(), location.getCity(), location.getDistrict(),
-                        location.getStation(), location.getAddress(), location.getPhone()),
-                Arrays.asList(locationNameField, cityField, districtField,
-                        subwayField, addressField, phoneField)
-        );
-
-        softAssert.assertAll();
+        Assert.assertEquals(location.getCity(),  expectedResult);
     }
 
 }
