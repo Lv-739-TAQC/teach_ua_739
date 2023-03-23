9b17b67a58c29ae4a74ce63434bd3b1ab44c7d71
Kapustin Ilya <63232432+kapustin-ilya@users.noreply.github.com>
2023-03-23 09:09:33 +0200

    Added documentation to main classes in db package (#158)
    Added in main classes in DB package

--- src/main/java/org/ssu/edu/teachua/db/annotation/TableDB.java
+++ src/main/java/org/ssu/edu/teachua/db/annotation/TableDB.java
@@ -5,8 +5,17 @@ import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
 import java.lang.annotation.Target;
 
+
+/**
+ * Specifies that the class is an entity. This annotation is applied to the entity class.
+ * @author Kapustin Illia
+ */
 @Retention(value = RetentionPolicy.RUNTIME)
 @Target(value = ElementType.TYPE)
 public @interface TableDB {
+	/**
+	 * The name table in database. 
+	 * This name is used to refer to the entity in database.
+	 */
     String name();
 }
--- src/main/java/org/ssu/edu/teachua/db/service/BaseService.java
+++ src/main/java/org/ssu/edu/teachua/db/service/BaseService.java
@@ -4,10 +4,23 @@ import org.ssu.edu.teachua.db.repository.DBException;
 import org.ssu.edu.teachua.db.repository.DBManager;
 
 import java.sql.Connection;
-
+/**
+ * An abstract class is the parent of all service
+ * @author User
+ *
+ */
 public abstract class BaseService {
+	/**
+	 * The field is connection database
+	 */
     protected Connection connection;
-
+    /**
+     * Constructs an BaseService creates connection.
+     * @param url full url in jdbc format
+	 * @param username username for connection
+	 * @param password password for connection 
+     * @throws DBException
+     */
     public BaseService(String url, String username, String password) throws DBException {
         this.connection = DBManager.getInstance(url, username, password).getConnection();
     }
