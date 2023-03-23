aff18dfdb6c66de4d5a742ca76dadf6e2b92ff63
Tafiy-Janet <113380541+Tafiy-Janet@users.noreply.github.com>
2023-03-23 10:49:34 +0200

    Added new test and javadoc comments (#160)
    * Added new test and javadoc comments
    
    * Renamed test method

--- src/main/java/org/ssu/edu/teachua/ui/pages/view/ViewCenterPage.java
+++ src/main/java/org/ssu/edu/teachua/ui/pages/view/ViewCenterPage.java
@@ -7,6 +7,13 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.ssu.edu.teachua.ui.base.BasePage;
 
+/**
+ * This class represents a page for viewing a specific center's information.
+ * It extends the BasePage class and contains web elements for the center name, description,
+ * enroll button, download button and map button. The class provides methods to interact with
+ * these web elements, such as clicking on the enroll and download buttons, and getting the
+ * center name and description.
+ */
 public class ViewCenterPage extends BasePage {
 
     @FindBy(how = How.XPATH, using = "//div[@class='name-box']/span[@class='center-name']")
@@ -20,33 +27,61 @@ public class ViewCenterPage extends BasePage {
     @FindBy(how = How.XPATH, using = "//a[contains(@href,'https://maps.google.com/maps')]")
     protected WebElement mapButton;
 
+    /**
+     * Constructor for the ViewCenterPage class.
+     *
+     * @param driver The WebDriver object used to interact with the web page.
+     */
     public ViewCenterPage(WebDriver driver) {
         super(driver);
     }
 
+    /**
+     * Clicks on the enroll button for the center.
+     *
+     * @return The current instance of the ViewCenterPage class.
+     */
     @Step("Click to enroll the center")
     public ViewCenterPage clickEnrollCenter() {
         enrollButton.click();
         return this;
     }
 
+    /**
+     * Clicks on the download button for the center.
+     *
+     * @return The current instance of the ViewCenterPage class.
+     */
     @Step("Click to download a file with information about the center")
     public ViewCenterPage downloadFile() {
         downloadButton.click();
         return this;
     }
 
+    /**
+     * Clicks on the map button to redirect to Google Maps.
+     */
     @Step("Click to redirect on map")
     public void clickOnMap() {
         mapButton.click();
     }
 
-   @Step("Get name of the center")
+    /**
+     * Gets the name of the center.
+     *
+     * @return A String representation of the center name.
+     */
+    @Step("Get name of the center")
     public String getCenterName() {
         return centerName.getText();
     }
 
-   @Step("Get description of the center")
+    /**
+     * Gets the description of the center.
+     *
+     * @return A String representation of the center description.
+     */
+    @Step("Get description of the center")
     public String getDescription() {
         return description.getText();
     }
--- src/main/java/org/ssu/edu/teachua/ui/pages/view/ViewClubPage.java
+++ src/main/java/org/ssu/edu/teachua/ui/pages/view/ViewClubPage.java
@@ -8,6 +8,11 @@ import org.openqa.selenium.support.How;
 import org.ssu.edu.teachua.ui.components.modal.FeedbackComponent;
 import org.ssu.edu.teachua.ui.components.modal.EnrollClubComponent;
 
+/**
+ * This class represents the page that displays information about a particular club.
+ * It extends the ViewCenterPage class.
+ * The class contains WebElements and methods for interacting with the elements on the page.
+ */
 public class ViewClubPage extends ViewCenterPage {
 
     @FindBy(how = How.XPATH, using = "//span[@class='club-name']")
@@ -33,51 +38,94 @@ public class ViewClubPage extends ViewCenterPage {
     )
     private WebElement subscribeButton;
 
+    /**
+     * Constructs a new ViewClubPage object with the given WebDriver.
+     *
+     * @param driver the WebDriver instance to use
+     */
     public ViewClubPage(WebDriver driver) {
         super(driver);
     }
 
-
-   @Step("Click to enroll the club")
-   public EnrollClubComponent clickEnrollClub() {
+    /**
+     * Clicks the enroll club button and returns a new instance of the EnrollClubComponent class.
+     *
+     * @return The EnrollClubComponent instance.
+     */
+    @Step("Click to enroll the club")
+    public EnrollClubComponent clickEnrollClub() {
         enrollButton.click();
         return new EnrollClubComponent(driver, enrollClubNode);
     }
 
+    /**
+     * Clicks the next club image button and returns the current instance of the ViewClubPage class.
+     *
+     * @return The current instance of the ViewClubPage class.
+     */
     @Step("Click to get next club image")
     public ViewClubPage clickOnNextImage() {
         nextImage.click();
         return this;
     }
 
+    /**
+     * Clicks the previous club image button and returns the current instance of the ViewClubPage class.
+     *
+     * @return The current instance of the ViewClubPage class.
+     */
     @Step("Click to get previous club image")
     public ViewClubPage clickOnPreviousImage() {
         previousImage.click();
         return this;
     }
+
+    /**
+     * Clicks the download file button and returns the current instance of the ViewClubPage class.
+     *
+     * @return The current instance of the ViewClubPage class.
+     */
     @Step("Click to download a file with information about the club")
     public ViewClubPage downloadFile() {
         downloadButton.click();
         return this;
     }
 
+    /**
+     * Clicks the leave feedback button and returns a new instance of the FeedbackComponent class.
+     *
+     * @return The FeedbackComponent instance.
+     */
     @Step("Click to leave a feedback about the club")
     public FeedbackComponent clickLeaveAFeedback() {
         feedbackButton.click();
         return new FeedbackComponent(driver, leaveFeedbackNode);
     }
 
+    /**
+     * Clicks on the link to redirect to the club's webpage.
+     */
     @Step("Click to redirect on club webpage")
     public void clickOnClubLink() {
         clubLink.click();
     }
 
+    /**
+     * Clicks on the button to get more details about the club.
+     *
+     * @return The ViewClubPage after clicking on the button.
+     */
     @Step("Click to get more details about the club")
     public ViewClubPage clickOnMoreDetails() {
         moreDetailsButton.click();
         return this;
     }
 
+    /**
+     * Clicks on the button to show all feedback
+     *
+     * @return The ViewClubPage after clicking on the button.
+     */
     @Step("Click to show all feedback")
     public ViewClubPage clickOnShowFeedback() {
         showFeedback.click();
@@ -88,6 +136,11 @@ public class ViewClubPage extends ViewCenterPage {
         return waitForElementToAppear(subscribeButton).isEnabled();
     }
 
+    /**
+     * Gets the name of the club.
+     *
+     * @return The name of the club.
+     */
     @Step("Get club name")
     public String getClubName() {
         return clubName.getText();
