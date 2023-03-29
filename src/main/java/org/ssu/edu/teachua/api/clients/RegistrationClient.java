9596e34093dda2d3494a150d675bc049af82021f
Zhanna Stadnytska <janelischuk@gmail.com>
2023-03-29 22:00:20 +0300

    realization of registrationClient

--- src/main/java/org/ssu/edu/teachua/api/clients/RegistrationClient.java
+++ src/main/java/org/ssu/edu/teachua/api/clients/RegistrationClient.java
@@ -1,10 +1,24 @@
 package org.ssu.edu.teachua.api.clients;
 
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.ssu.edu.teachua.api.models.registration.RegistrationRequest;
 
-public class RegistrationClient extends BaseClient{
+public class RegistrationClient extends BaseClient {
 
-    public RegistrationClient(String url, ContentType contentType, String accessToken) {
-        super(url, contentType, accessToken);
+    private final String path = "/api/signup";
+
+    public RegistrationClient(String url, ContentType contentType) {
+        super(url, contentType);
     }
+
+    public Response registerUser(int id, String firstName, String lastName, String email, String password, String phone, String roleName,
+                                 String verificationCode, String urlLogo, boolean status) {
+        RegistrationRequest request = new RegistrationRequest(firstName, lastName, email, password, phone, roleName);
+        return prepareRequest()
+                .body(request)
+                .when()
+                .post(path);
+    }
+
 }
--- src/main/java/org/ssu/edu/teachua/api/models/login/SignInRequest.java
+++ src/main/java/org/ssu/edu/teachua/api/models/login/SignInRequest.java
@@ -5,7 +5,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class SingInRequest {
+public class SignInRequest {
     public String email;
     public String password;
 }
