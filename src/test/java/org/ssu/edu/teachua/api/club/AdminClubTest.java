b49e4df1aa5d1f191fa7d991daa5bfd0a34038a2
Zhanna Stadnytska <janelischuk@gmail.com>
2023-03-30 17:06:58 +0300

    fix2

--- src/test/java/org/ssu/edu/teachua/api/club/AdminClubTest.java
+++ src/test/java/org/ssu/edu/teachua/api/club/AdminClubTest.java
@@ -2,5 +2,5 @@ package org.ssu.edu.teachua.api.club;
 
 import org.ssu.edu.teachua.utils.runners.LoginWithAdminAPIRunner;
 
-public class ClubTest extends LoginWithAdminAPIRunner {
+public class AdminClubTest extends LoginWithAdminAPIRunner {
 }
