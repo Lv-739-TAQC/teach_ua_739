8c86a3083ca8b2321b90e0e0f24067588b38fd4c
mtvnk <31481739+AnnaMatviienko@users.noreply.github.com>
2023-03-31 15:42:05 +0300

    Club CRUD/Response/Request - review is required (#171)
    * Club CRUD/Response/Request - review is required
    
    * Refactored Club CRUD/Response/Request - review is required
    
    * Refactored New Club CRUD/Response/Request - review is required
    
    * Refactored-3 Club CRUD/Response/Request - review is required
    
    * Refactored-4 Club CRUD/Response/Request - review is required
    
    * Refactored-5 Club CRUD/Response/Request

--- src/main/java/org/ssu/edu/teachua/api/models/club/ClubRequest.java
+++ src/main/java/org/ssu/edu/teachua/api/models/club/ClubRequest.java
@@ -1,4 +1,31 @@
 package org.ssu.edu.teachua.api.models.club;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import org.ssu.edu.teachua.api.models.location.Location;
+import org.ssu.edu.teachua.api.models.url_gallery.UrlGallery;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
 public class ClubRequest {
+    private int id;
+    private String name;
+    private String description;
+    private int centerId;
+    private ArrayList<String> categoriesName;
+    private ArrayList<Location> locations;
+    private int ageFrom;
+    private int ageTo;
+    private String urlBackground;
+    private String urlLogo;
+    private ArrayList<UrlGallery> urlGallery;
+    private boolean isOnline;
+    private String contacts;
+    private boolean isApproved;
+    private int userId;
+    private int clubExternalId;
+    private int centerExternalId;
+
 }
