89467aa4266cf850c4080504d7ee95a8a2b1b28c
Maksik <maksym.oliinyk.knm.2020@lpnu.ua>
2023-03-31 15:27:31 +0300

    Implementation of TUA-415

--- src/test/java/org/ssu/edu/teachua/api/profile/UserProfileTest.java
+++ src/test/java/org/ssu/edu/teachua/api/profile/UserProfileTest.java
@@ -1,6 +1,45 @@
 package org.ssu.edu.teachua.api.profile;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Issue;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import io.restassured.http.ContentType;
+import org.ssu.edu.teachua.api.clients.ProfileClient;
+import org.ssu.edu.teachua.api.models.error.ErrorResponse;
+import org.ssu.edu.teachua.api.models.profile.ProfilePutRequest;
+import org.ssu.edu.teachua.utils.providers.DataProviderProfilePage;
 import org.ssu.edu.teachua.utils.runners.LoginWithUserAPIRunner;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
 
 public class UserProfileTest extends LoginWithUserAPIRunner {
+
+    private ProfileClient client;
+
+    @BeforeMethod
+    public void initClient() {
+        client = new ProfileClient(valueProvider.getBaseUiUrl(), ContentType.JSON, accessToken);
+    }
+
+    @Issue("TUA-415")
+    @Severity(SeverityLevel.CRITICAL)
+    @Description("This test case verifies that user can not save changes with invalid" +
+                 "\n data where edit profile (fields lastName and firstName)")
+    @Test(dataProvider = "dpTestUpdateFirstLastNamesInvalid", dataProviderClass = DataProviderProfilePage.class)
+    public void testUpdateFirstLastNamesInvalid(int id, String firstName, String lastName, String email,
+                                                String phone, String roleName, String urlLogo, boolean status,
+                                                int expectedStatusCode, String expectedErrorMsg) {
+        SoftAssert dpSoftAssert = new SoftAssert();
+
+        ProfilePutRequest profilePutRequest = new ProfilePutRequest(
+                firstName, lastName, email, phone, roleName, urlLogo, status
+        );
+        ErrorResponse errorResponse = client.updateProfile(id, profilePutRequest).as(ErrorResponse.class);
+
+        dpSoftAssert.assertEquals(errorResponse.getStatus(), expectedStatusCode);
+        dpSoftAssert.assertEquals(errorResponse.getMessage(), expectedErrorMsg);
+        dpSoftAssert.assertAll();
+    }
 }
--- src/test/java/org/ssu/edu/teachua/utils/providers/DataProviderProfilePage.java
+++ src/test/java/org/ssu/edu/teachua/utils/providers/DataProviderProfilePage.java
@@ -11,9 +11,9 @@ public class DataProviderProfilePage {
                         "Будь ласка, введіть новий пароль", "Будь ласка, введіть діючий пароль"}
         };
     }
-    
+
     @DataProvider(name = "dpTestVerifieDataLastName")
-    private Object[][] dpTestVerifieDataLastName() {
+    private static Object[][] dpTestVerifieDataLastName() {
         return new Object[][]{
                 {"AfBbCcDdEeFfGgHhIiJjKkLlMm", "Прізвище не може містити більше, ніж 25 символів"},
                 {"!@#$%^&,", "Прізвище не може містити спеціальні символи"},
@@ -27,16 +27,34 @@ public class DataProviderProfilePage {
                 {"", "Введіть прізвище"}
         };
     }
-    
+
     @DataProvider(name = "dpTestVerifieDataPhoneNumber")
-	private Object[][] dpTestVerifieDataNumberPhone() {
-		return new Object[][] { 
-				{ "+3806895", "Телефон не відповідає формату +38(___) ___ __ __" },
-				{ "+38065987458", "Телефон не відповідає формату +38(___) ___ __ __" },
-				{ "+3806593859632586", "Телефон не відповідає формату +38(___) ___ __ __" },
-				{ "+3806598521475", "Телефон не відповідає формату +38(___) ___ __ __" },
-				{ "jngeoлщшогнеп", "Телефон не відповідає формату +38(___) ___ __ __" },
-				{ "!@#$%^&*(_+.:", "Телефон не відповідає формату +38(___) ___ __ __" },
-				{ "", "Телефон не відповідає формату +38(___) ___ __ __" } };
-	}
+    private static Object[][] dpTestVerifieDataNumberPhone() {
+        return new Object[][]{
+                {"+3806895", "Телефон не відповідає формату +38(___) ___ __ __"},
+                {"+38065987458", "Телефон не відповідає формату +38(___) ___ __ __"},
+                {"+3806593859632586", "Телефон не відповідає формату +38(___) ___ __ __"},
+                {"+3806598521475", "Телефон не відповідає формату +38(___) ___ __ __"},
+                {"jngeoлщшогнеп", "Телефон не відповідає формату +38(___) ___ __ __"},
+                {"!@#$%^&*(_+.:", "Телефон не відповідає формату +38(___) ___ __ __"},
+                {"", "Телефон не відповідає формату +38(___) ___ __ __"}};
+    }
+
+    @DataProvider(name = "dpTestUpdateFirstLastNamesInvalid")
+    private static Object[][] dpTestUpdateFirstLastNamesInvalid() {
+        return new Object[][]{
+                {203, "Nastia1234", "Kukh", "999999922", "soyec48727@busantei.com", "ROLE_MANAGER", null, true,
+                        400, ("\"firstName\"" + " can`t contain numbers")},
+                {203, "NastiaNastiaNastiaNastiaNastia", "Kukh", "999999922", "soyec48727@busantei.com", "ROLE_MANAGER", null, true,
+                        400, ("\"firstName\"" + " can contain from 1 to 25 letters")},
+                {203, "Nastia!@##$#$%", "Kukh", "999999922", "soyec48727@busantei.com", "ROLE_MANAGER", null, true,
+                        400, ("\"firstName\"" + " can contain only ukrainian and english letters")},
+                {203, "Nastia", "Kukhar#%$#", "999999922", "soyec48727@busantei.com", "ROLE_MANAGER", null, true,
+                        400, ("\"lastName\"" + " can contain only ukrainian and english letters")},
+                {203, "Nastia", "KukharKukharKukharKukharKukharKukharKukharK", "999999922", "soyec48727@busantei.com", "ROLE_MANAGER", null, true,
+                        400, ("\"lastName\"" + " can contain from 1 to 25 letters")},
+                {203, "Nastia", "Kukhar123343#", "999999922", "soyec48727@busantei.com", "ROLE_MANAGER", null, true,
+                        400, ("\"lastName\"" + " can`t contain numbers")},
+        };
+    }
 }
