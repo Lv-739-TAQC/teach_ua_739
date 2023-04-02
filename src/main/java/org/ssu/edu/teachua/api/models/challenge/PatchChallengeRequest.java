185ecc6ba5684f237f315b888c1d9df0ac65ed8a
kapustinILya <k.ilya.v@gmail.com>
2023-04-02 12:42:32 +0300

    Updated ChallengeClient, created two class for challenge request

---
+++ src/main/java/org/ssu/edu/teachua/api/models/challenge/PatchChallengeRequest.java
@@ -0,0 +1,12 @@
+package org.ssu.edu.teachua.api.models.challenge;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class PatchChallengeRequest {
+	private String name;
+	private int sortNumber;
+    private String title;
+}
