fb3d21c7f9811dfc5600f75c0513e9990b413fb6
Zhanna Stadnytska <janelischuk@gmail.com>
2023-04-04 15:51:35 +0300

    challengeclient

--- src/main/java/org/ssu/edu/teachua/api/clients/ChallengeClient.java
+++ src/main/java/org/ssu/edu/teachua/api/clients/ChallengeClient.java
@@ -1,10 +1,22 @@
 package org.ssu.edu.teachua.api.clients;
 
+import io.qameta.allure.Step;
 import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.ssu.edu.teachua.api.models.challenge.PostChallengeRequest;
 
 public class ChallengeClient extends BaseClient{
 
+    private final String path = "/api/challenge";
+
     public ChallengeClient(String url, ContentType contentType, String accessToken) {
         super(url, contentType, accessToken);
+    }
+    @Step("Create a request for creating new challenge entity using 'POST' method. Enter values in 'Body': {request}")
+    public Response createChallenge(PostChallengeRequest request) {
+        return prepareRequest()
+                .body(request)
+                .when()
+                .post(path);
     }
 }
