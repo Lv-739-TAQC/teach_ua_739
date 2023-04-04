89dfe7139a8b44710b2877b5ec2da0891ab976f0
Janet <janet.tafiy@gmail.com>
2023-04-04 21:49:36 +0300

    Added center client and modal

---
+++ src/main/java/org/ssu/edu/teachua/api/models/center/CenterRequest.java
@@ -0,0 +1,22 @@
+package org.ssu.edu.teachua.api.models.center;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.ArrayList;
+
+@Data
+@AllArgsConstructor
+public class CenterRequest {
+    private int id;
+    private String name;
+    private ArrayList<String> locations;
+    private String description;
+    private String urlWeb;
+    private String urlBackgroundPicture;
+    private String urlLogo;
+    private ArrayList<String> clubsId;
+    private int userId;
+    private String contacts;
+    private int centerExternalId;
+}
