40fdf71afe0142a4bdb7c6e7fcd7d97be7528182
TetianaFilatova <t.filatova0564@gmail.com>
2023-04-13 12:57:03 +0300

    add cucumber test

--- src/test/java/org/ssu/edu/teachua/cucumber/TestRunner.java
+++ src/test/java/org/ssu/edu/teachua/cucumber/TestRunner.java
@@ -1,32 +1,28 @@
 package org.ssu.edu.teachua.cucumber;
 
-import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
-import org.junit.runner.RunWith;
 import io.cucumber.testng.*;
 import org.ssu.edu.teachua.utils.Retry;
 import org.testng.ITestContext;
 import org.testng.ITestNGMethod;
 import org.testng.annotations.*;
 
-@RunWith(Cucumber.class)
-@CucumberOptions(
-        features = "src/test/resources/features",
+@CucumberOptions(features = "src/test/resources/features/tua71.feature",
         glue = "org.ssu.edu.teachua.cucumber.steps")
 public class TestRunner extends AbstractTestNGCucumberTests {
     private TestNGCucumberRunner testNGCucumberRunner;
 
     @BeforeSuite(alwaysRun = true)
-    public void beforeSuite(ITestContext context){
-        for(ITestNGMethod method : context.getAllTestMethods()){
+    public void beforeSuite(ITestContext context) {
+        for (ITestNGMethod method : context.getAllTestMethods()) {
             method.setRetryAnalyzerClass(Retry.class);
         }
     }
 
-    @BeforeClass(alwaysRun=true)
-    public void setUpClass(){
+    @BeforeClass(alwaysRun = true)
+    public void setUpClass() {
         testNGCucumberRunner = new TestNGCucumberRunner(this.getClass());
     }
+
     @Test(description = "Example of BDD suite", dataProvider = "scenarios")
     public void scenario(PickleWrapper pickle, FeatureWrapper cucumberFeature) {
         testNGCucumberRunner.runScenario(pickle.getPickle());
---
+++ src/test/resources/features/tua146.feature
@@ -0,0 +1,10 @@
+Feature: TUA-146 scenario
+
+  Background: Open website speak-ukrainian.org.ua/dev/
+    Given User open website home page
+
+  Scenario: Verify that 'Новини' blocks are in descending order
+    When User open 'Новини' page
+    And User get all dates from news cards and add them to list
+    And User sort all this dates by desc order and add them to new list
+    Then First list equal to second list
